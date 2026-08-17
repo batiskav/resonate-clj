d73f4c569bb9808949dbd3d9f0bd330bc6077001
Thomas Moerman <thomas.moerman+gh@gmail.com>
2026-08-17 19:26:52 +0200

    neil lib initialization

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to resonate-clj/resonate-clj
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/resonate_clj/resonate_clj_test.clj
@@ -0,0 +1,7 @@
+(ns resonate-clj.resonate-clj-test
+  (:require [clojure.test :refer :all]
+            [resonate-clj.resonate-clj :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
