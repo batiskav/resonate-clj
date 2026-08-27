e7fa12c806dbe8781cc27e3b5b91a649b36032f2
Thomas Moerman <thomas.moerman@gmail.com>
2026-08-27 15:14:56 +0200

    user ns for REPL interaction

---
+++ dev/user.clj
@@ -0,0 +1,37 @@
+(ns user
+  "REPL entry point."
+  (:require
+   [clojure.java.io :as io]
+   [clojure.repl.deps :as deps]
+   [clojure.test :as test]
+   [clojure.tools.namespace.repl :as repl]
+   [resonate.resonate :as r]
+   [resonate.greeter-test :as g])
+  (:import 
+   [io.resonatehq.resonate Retry$Never]))
+
+(comment 
+  
+  (require '[resonate.resonate :as r] :reload)
+  (require '[resonate.greeter-test :as g] :reload)
+
+  (test/run-tests 'resonate.greeter-test)
+  
+  (def R (r/start! {:url "http://localhost:8001"}))
+  (r/register R #'g/format-greeting)
+  (r/register R #'g/greeter-workflow)
+  
+  (try 
+    (-> (r/run-new R #'g/greeter-workflow "Echo")
+        (r/result))
+    (catch Exception e
+      (.printStackTrace e)))
+  
+  (r/stop! R)
+  
+  (repl/clear)
+  (repl/refresh-all)
+
+  (r/method-of #'g/greeter-workflow) 
+
+  )
